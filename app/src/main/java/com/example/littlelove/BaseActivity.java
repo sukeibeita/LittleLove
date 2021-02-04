9412bc2a11ceabcac3932989eb2268a6d6903edd
Suke <210915219@qq.com>
2021-02-04 23:21:52 +0800

    add BaseActivity

---
+++ app/src/main/java/com/example/littlelove/BaseActivity.java
@@ -0,0 +1,21 @@
+package com.example.littlelove;
+
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("BaseActivity", getClass().getSimpleName());
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.remove(this);
+    }
+}
--- app/src/main/java/com/example/littlelove/MainActivity.java
+++ app/src/main/java/com/example/littlelove/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.littlelove;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
